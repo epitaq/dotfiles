666a08832fc1b24b6670c6b6059845f57a5807e6
epita <somaok2004@gmail.com>
2024-03-30 22:40:46 +0900

    add: vscode settings

--- home-manager/desktop/vscode/default.nix
+++ home-manager/desktop/vscode/default.nix
@@ -6,10 +6,19 @@
   };
   programs.vscode = {
     enable = true;
+    package = pkgs.vscodium;
+
+    mutableExtensionsDir = false;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+
     extensions = with pkgs.vscode-marketplace; [
       ms-ceintl.vscode-language-pack-ja
       pkief.material-icon-theme
+      enkia.tokyo-night
     ];
+
+    userSettings = builtins.readFile ./settings.json;
   };
 }
 
